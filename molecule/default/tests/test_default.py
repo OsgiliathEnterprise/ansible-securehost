89b959f256f4fd064ff0b84a49f2f3f37686a509
Charlie Mordant <cmordant1@gmail.com>
2020-05-03 14:45:00 +0200

    lint

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -14,12 +14,14 @@ def test_fail2ban_is_running(host):
 
 
 def test_challenge_response_authentication_is_disabled(host):
-    command = """egrep -q '^ChallengeResponseAuthentication no$' /etc/ssh/sshd_config && echo 1 || echo 0"""
+    command = """egrep -q '^ChallengeResponseAuthentication no$' \
+    /etc/ssh/sshd_config && echo 1 || echo 0"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
 
 
 def test_challenge_password_authentication_is_disabled(host):
-    command = """egrep -q '^PasswordAuthentication no$' /etc/ssh/sshd_config && echo 1 || echo 0"""
+    command = """egrep -q '^PasswordAuthentication no$' \
+    /etc/ssh/sshd_config && echo 1 || echo 0"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
